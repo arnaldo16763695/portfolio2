450b4086258d7142c64210e26cf1c3e3959e8745
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2024-09-11 12:24:47 -0400

    this commit works in header component

---
+++ components/Logo.jsx
@@ -0,0 +1,10 @@
+import  Image  from 'next/image'
+import Link from 'next/link'
+
+const Logo = () => {
+    return (
+        <Link href='/' ><Image src='/logo.svg' alt='logo' width={54} height={54} priority /></Link>
+    )
+}
+
+export default Logo
---
+++ components/Nav.jsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { motion } from 'framer-motion';
+
+const links = [
+    { path: '/', name: "home" },
+    { path: '/projects', name: "mis proyectos" },
+    { path: '/contact', name: "contacto" },
+]
+
+const Nav = ({ containerStyles, linkStyles, underlineStyles }) => {
+    const pathName = usePathname();
+    return (
+        <nav className={`${containerStyles}`}>
+            {links.map((link, index) => (
+                <Link href={link.path} key={index} className={`capitalize ${linkStyles}`} >
+                    {link.path === pathName && (
+                        <motion.span
+                            initial={{ y: '-100%' }}
+                            animate={{ y: 0 }}
+                            transition={{ type: 'tween' }}
+                            layoutId='underline'
+                            className={ `${underlineStyles}` }
+                        />
+                    )}
+                    {link.name}
+                </Link>
+            ))}
+        </nav>
+    )
+}
+
+export default Nav
