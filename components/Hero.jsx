a9f6b821b0504a8999a8b468106a7818dbc211a5
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2024-09-16 09:59:30 -0400

    this commit works in badges

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -35,6 +35,17 @@ const Hero = () => {
           </div>
           {/* image */}
           <div className="hidden xl:flex relative">
+
+            {/* badge 1 */}
+            <Badge icon={<RiBriefcase4Fill />} endCountNum={3} badgeText={'Años de experiencia'} containerStyles='absolute top-[24%] -left-[5rem]' />
+            
+            {/* badge 2*/}
+            <Badge icon={<RiTodoFill />} endCountNum={6} endCountText={''} badgeText={'Proyectos terminados'} containerStyles='absolute top-[80%] -left-[1rem]' />
+           
+            {/* badge 3*/}
+            <Badge icon={<RiTeamFill />} endCountNum={6} endCountText={''} badgeText={'Clientes contentos'} containerStyles='absolute top-[55%] -right-8' />
+
+
             <div className='bg-hero_shape2_light dark:bg-hero_shape2_dark
              h-[500px] w-[500px] bg-no-repeat absolute -top-1 -right-2'>
             </div>
