42ff211f443ce5be1d440d1583c6befecd331c24
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2024-09-17 19:07:00 -0400

    this commit continues in the about component

--- components/About.jsx
+++ components/About.jsx
@@ -1,9 +1,9 @@
 import DevImg from '@/components/DevImg';
 import Image from 'next/image';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import {User2, MailIcon, HomeIcon, PhoneCall, GraduationCap, Calendar, Briefcase } from 'lucide-react'
+import { User2, MailIcon, HomeIcon, PhoneCall, GraduationCap, Calendar, Briefcase } from 'lucide-react'
 
-const infoDate = [
+const infoData = [
     {
         icon: <User2 size={20} />,
         text: 'Arnaldo Espinoza',
@@ -30,10 +30,10 @@ const infoDate = [
     },
 ]
 
-const qualificationData =[
+const qualificationData = [
     {
         title: 'education',
-        data:[
+        data: [
             {
                 univerity: "Carlos Soublette",
                 qualification: "TSU en Sistemas de Información",
@@ -49,7 +49,7 @@ const qualificationData =[
     },
     {
         title: 'experience',
-        data:[
+        data: [
             {
                 company: "Makro",
                 qualification: "Operador Soporte Técnico",
@@ -60,7 +60,7 @@ const qualificationData =[
                 qualification: "instructor CCNA",
                 year: '2014-2016'
             },
-           
+
         ]
 
     },
@@ -99,20 +99,57 @@ const skillData = [
             {
                 imgPath: '/about/wordpress.svg'
             },
-           
+
         ]
     }
 ]
 
 const About = () => {
-    const getData = (arr, title) =>{
-        return arr.find((item)=> item.title === title);
+    const getData = (arr, title) => {
+        return arr.find((item) => item.title === title);
     }
     console.log(getData(qualificationData, 'education'))
     return (
-        <section>
+        <section className='xl:h-[860px] pb-12 xl:py-24'>
             <div className='container mx-auto'>
-                <h2 className='section-title'>Acerca de mi</h2>
+                <h2 className='section-title mb-8 xl:mb-16 text-center mx-auto'>Acerca de mi</h2>
+                <div className='flex flex-col xl:flex-row'>
+                    {/* image */}
+                    <div className='hidden xl:flex flex-1 relative'>
+                        <DevImg containerStyles='bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-no-repeat relative' imgSrc='/about/developer.png' />
+                    </div>
+                    {/* tabs  */}
+                    <div className='flex-1'>
+                        <Tabs defaultValue='personal'>
+                            <TabsList className='w-full grid xl:grid-cols-3 xl:max-w-[520px] xl:border dark:border-none'>
+                                <TabsTrigger className='w-[190px] xl:w-auto' value='personal'>Información</TabsTrigger>
+                                <TabsTrigger className='w-[190px] xl:w-auto' value='qualifications'>Qualifications</TabsTrigger>
+                                <TabsTrigger className='w-[190px] xl:w-auto' value='skills'>Skills</TabsTrigger>
+                            </TabsList>
+                            {/* tabs content */}
+                            <div className='text-lg mt-12 xl:mt-8'>
+                                {/* personal */}
+                                <TabsContent value='personal'>
+                                    <div className='pl-4 text-center xl:text-left'>
+                                        <h3 className='h3 mb-4'>Unmatched Service Quality for over 10 Years</h3>
+                                        <p className='subtitle max-w-xl mx-auto xl:mx-0'>Me especializo en crear sitios web intuitivos con tecnología de punta, brindando experiencias de usuario dinámicas y atractivas </p>
+                                        {/* icons */}
+                                        <div className='grid xl:grid-cols-2 gap-4 mb-12'>
+                                            {infoData.map((item, index) => (
+                                                <div  className='flex items-center gap-x-4 mx-auto xl:mx-0' key={index}>
+                                                    <div className='text-primary'>{item.icon}</div>
+                                                    <div>{item.text}</div>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </TabsContent>
+                                <TabsContent value='qualifications'>qualifications</TabsContent>
+                                <TabsContent value='skills'>skills</TabsContent>
+                            </div>
+                        </Tabs>
+                    </div>
+                </div>
             </div>
         </section>
     )
