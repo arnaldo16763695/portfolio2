e73f1f44909e77f849d250ca3fd8d2d8a2290087
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2024-09-18 15:42:09 -0400

    this git set Service component

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import Services from "@/components/Services";
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main>
       <Hero />
       <About />
+      <Services />
     </main>
   );
 }
