4de46de08b439ce65243b5d4776d5cea5b898035
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2024-09-19 10:08:29 -0400

    this commit set Reviews component

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,6 +1,6 @@
 import { Github, Link2Icon } from 'lucide-react';
 import { Badge } from '@/components/ui/badge';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardHeader } from '@/components/ui/card';
 import Image from 'next/image';
 import Link from 'next/link';
 
